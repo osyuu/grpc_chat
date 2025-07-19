ef11fe62accb9f9fd44bf9d14adf5199c7b5f62b
Phil <lf217189@gmail.com>
2025-07-19 19:03:39 +0800

    chore(backend): first commit

---
+++ backend/chat_server/bin/chat_server.dart
@@ -0,0 +1,5 @@
+import 'package:chat_server/chat_server.dart' as chat_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${chat_server.calculate()}!');
+}
---
+++ backend/chat_server/test/chat_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:chat_server/chat_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
