8e76552a6fd57ba629eaa06d42fad69fb37a086b
Phil <lf217189@gmail.com>
2025-07-20 11:28:40 +0800

    feat(backend): implement gRPC chat server with message handling and timestamp support

--- backend/chat_server/bin/chat_server.dart
+++ backend/chat_server/bin/chat_server.dart
@@ -1,5 +1,50 @@
-import 'package:chat_server/chat_server.dart' as chat_server;
+import 'dart:async';
 
-void main(List<String> arguments) {
-  print('Hello world: ${chat_server.calculate()}!');
+import 'package:chat_server/src/generated/chat.pbgrpc.dart';
+import 'package:chat_server/src/generated/google/protobuf/timestamp.pb.dart';
+import 'package:grpc/grpc.dart';
+import 'package:uuid/uuid.dart';
+
+Future<void> main() async {
+  final server = Server.create(
+    services: [ChatServer()],
+    codecRegistry: CodecRegistry(codecs: const [GzipCodec(), IdentityCodec()]),
+  );
+
+  await server.serve(port: 50051);
+
+  print('Server listening on port ${server.port}...');
+}
+
+class ChatServer extends ChatServiceBase {
+  final _controllers = <StreamController<Message>, void>{};
+
+  @override
+  Stream<Message> connect(ServiceCall call, Stream<Message> request) async* {
+    final clientController = StreamController<Message>();
+    _controllers[clientController] = null;
+
+    request.listen((request) {
+      print('Connected: ${request.sender} #${request.hashCode}');
+
+      _controllers.forEach((controller, _) {
+        if (controller != clientController) {
+          controller.sink.add(request);
+        }
+      });
+    }).onError((e) {
+      print(e);
+
+      _controllers.remove(clientController);
+      clientController.close();
+      print('Disconnected: #${request.hashCode}');
+    });
+
+    await for (var message in clientController.stream) {
+      print('Received message: ${message.content}');
+      message.id = Uuid().v1();
+      message.createdAt = Timestamp.fromDateTime(DateTime.now());
+      yield message;
+    }
+  }
 }
